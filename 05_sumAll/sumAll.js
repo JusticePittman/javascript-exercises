0c2a54ba116df1c243c3b0e48a132b2997496966
Justice Pittman <justice.a.pittman@gmail.com>
2022-01-11 07:40:51 -0500

    Updated 05_sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,14 @@
-const sumAll = function(num1, num2) {
+const sumAll = function() {
     let sum = 0;
-    let min = Math.min(arguments[0], arguments[1]);
-    let max = Math.max(arguments[0], arguments[1]);
-    
-    if((Number.isInteger(num1) && Number.isInteger(num2)) && (min >= 0 && max >= 0)){
-        for( let i = min; i <= max; i++){
+    let range = Array.from(arguments);
+    if((range.every(value => Number.isInteger(value))) && (range.every(value => value >= 0))){
+        for( let i = Math.min(...range); i <= Math.max(...range); i++){
             sum += i;
         };
         return sum;
     } else {
         return 'ERROR';
     };
-
-    
-
-    console.log(sum);
 };
-sumAll(1, [1,2,3]);
 // Do not edit below this line
 module.exports = sumAll;
