dee9019caf76956fd4e0a3c1a0d203398f2d9721
Justice Pittman <justice.a.pittman@gmail.com>
2022-01-11 06:27:20 -0500

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let min = Math.min(arguments[0], arguments[1]);
+    let max = Math.max(arguments[0], arguments[1]);
+    
+    if((Number.isInteger(num1) && Number.isInteger(num2)) && (min >= 0 && max >= 0)){
+        for( let i = min; i <= max; i++){
+            sum += i;
+        };
+        return sum;
+    } else {
+        return 'ERROR';
+    };
 
+    
+
+    console.log(sum);
 };
-
+sumAll(1, [1,2,3]);
 // Do not edit below this line
 module.exports = sumAll;
